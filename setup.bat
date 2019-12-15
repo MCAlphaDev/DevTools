ec3d2bca827f5f8c97233740a63ced75e37c8592
valoeghese <valoeghesemods@gmail.com>
2019-12-15 21:09:30 +1300

    decompile minecraft

--- setup.bat
+++ setup.bat
@@ -1,6 +1,9 @@
 @echo off
 py scripts/mclib_retriever.py
+echo remapping client jar
 java -jar tiny-remapper.jar libs/client.jar libs/client-mapped.jar data/client.tiny proguard named
 cd libs
 del client.jar
 cd ..
+echo decompiling minecraft
+java -jar procyon.jar -jar libs/client-mapped.jar -o libs/client-sources/ --suppress-banner
