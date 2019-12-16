7599bfcaffb8cc0e53d76d0a6a52055866ddd6ed
valoeghese <valoeghesemods@gmail.com>
2019-12-16 20:48:34 +1300

    Replace old mappings when generating sources

--- setup.bat
+++ setup.bat
@@ -4,6 +4,7 @@ echo remapping client jar
 java -jar tiny-remapper.jar libs/client.jar libs/client-mapped.jar data/client.tiny proguard named
 cd libs
 del client.jar
+rd /S /Q "client-sources/"
 cd ..
 echo decompiling minecraft
 java -jar procyon.jar -jar libs/client-mapped.jar -o libs/client-sources/ --suppress-banner
