3b5172dd48e36d0232b5db7bb6696ac9f7b599ad
valoeghese <valoeghesemods@gmail.com>
2020-05-02 23:00:45 +1200

    callbackinfo

--- src/io/github/mcalphadev/mixin/LevelMixin.java
+++ src/io/github/mcalphadev/mixin/LevelMixin.java
@@ -3,13 +3,14 @@ package io.github.mcalphadev.mixin;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import net.minecraft.level.Level;
 
 @Mixin(Level.class)
 public class LevelMixin {
 	@Inject(at = @At("RETURN"), method = "<init>")
-	private void onCreate(final String name) {
+	private void onCreate(final String name, CallbackInfo info) {
 		System.out.println("[Test] This constructor is used!");
 	}
 }
