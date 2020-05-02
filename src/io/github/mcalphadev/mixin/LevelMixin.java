024246fec169f64ebb2caaeff8382e3839fb5671
valoeghese <valoeghesemods@gmail.com>
2020-05-02 22:57:45 +1200

    start working on the registry remapping

--- src/io/github/mcalphadev/mixin/LevelMixin.java
+++ src/io/github/mcalphadev/mixin/LevelMixin.java
@@ -1,10 +1,15 @@
 package io.github.mcalphadev.mixin;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 
 import net.minecraft.level.Level;
 
 @Mixin(Level.class)
 public class LevelMixin {
-
+	@Inject(at = @At("RETURN"), method = "<init>")
+	private void onCreate(final String name) {
+		System.out.println("[Test] This constructor is used!");
+	}
 }
